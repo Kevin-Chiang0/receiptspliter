eaf65deb1cf5a518531e353ab476862f8fdc76cb
Ketarak <kc998877@gmail.com>
2022-05-27 12:36:21 -0400

    create json in cleanToJson

--- readReceipt.py
+++ readReceipt.py
@@ -15,28 +15,47 @@ def processImage(image):
         tax = re.findall(r"(\d+[.,]\d+[%])", file)
     # file = open("receiptText/temp.txt", mode="w")
     # file.write(totext)
-    cleanToJson(allFloat, date, tax)
+    cleanToJson(allFloat, date[0], tax[0])
 
 #TODO set up JSON TIP: rsplit to separate name and cost
 #Cleans data and returns JSON file
-def cleanToJson(allFloat, date, tax):    
+def cleanToJson(allFloat, date, tax):
+    taxcalc =  1 + float(tax.strip("%"))/100  
     banned = ["VISA", "CHANGE", "TOTAL", "TAX", "AMOUNT", "BOTTLE", "MASTERCARD", "DISCOVER", "CHASE", "CAPITALONE"]
     items = []
-    cleanedFloat = [entry.replace(",",".") for entry in allFloat]
-    for item in range(len(cleanedFloat)):
-        if cleanedFloat[item][-1] == "A":
-            items.append(addBottleTax(cleanedFloat[item],cleanedFloat[item+1]))
-            continue
-        if not any(bword in cleanedFloat[item] for bword in banned):
-            items.append(cleanedFloat[item])
-    print(items)
+    cost = []
+
+    cleanedList = [entry.replace(",",".") for entry in allFloat]
+
+    for item in range(len(cleanedList)):
+
+        if not any(bword in cleanedList[item] for bword in banned):
+            if cleanedList[item][-1] == "A":
+                cleanedList[item] = (addBottleTax(cleanedList[item],cleanedList[item+1]))
+            splitItems = cleanedList[item].split()
+            if splitItems[0] == "E":
+                items.append(" ".join(splitItems[2:-1]))
+                cost.append(round(float(splitItems[-1]) * taxcalc,2))
+            else:
+                items.append(" ".join(splitItems[1:-1]))
+                cost.append(float(splitItems[-1]))
+
+    jsonDict = {"items": items,
+                "cost": cost,
+                "tax": tax,
+                "date": date}
+
+    with open("./receiptText/receipt.json","w") as outfile:
+        json.dump(jsonDict, outfile)
+
 
 #Combines drink cost and bottle tax
 def addBottleTax(drink, canTax):
-    temp1 = drink.split()
-    temp2 = canTax.split()
-    return (" ".join(temp1[:-2]) + " " + str(float(temp1[-2]) + float(temp2[-1])))    
+    splitItems1 = drink.split()
+    splitItems2 = canTax.split()
+    combinedcost = float(splitItems1[-2]) + float(splitItems2[-1])
+    return " ".join(splitItems1[:-2]) + " " + str(combinedcost)
 
 # img = "./receiptImage/receipt.jpg"
-img = "./receiptText/temp.txt"
+img = "./receiptText/splitItems.txt"
 processImage(img)
